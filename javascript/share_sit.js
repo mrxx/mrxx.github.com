a6386af8b7d8de261c4595ea15ea518cb10f416b
mrxx <kids.lee@gmail.com>
2012-07-22 13:09:37 +0800

    done

---
+++ javascript/share_sit.js
@@ -0,0 +1,13 @@
+var sites = [
+  { name : "体感游戏", url : "http://mrxx.github.com", img : "http://mrxx.github.com/image/game.png" },
+];
+
+hostRegexp = new RegExp(location.host);
+for(var i = 0; i < sites.length; i++){
+  if(hostRegexp.test(sites[i].url)){
+    sites.splice(i,1);
+  }
+}
+idx = parseInt(Math.random() * 10,10) % sites.length;
+site = sites[idx];
+document.write("<h3>"+site.name+"</h3><div class='rq_relation'> <div class='img'><a href='"+ site.url +"' target='_blank'><img src='"+ site.img +"' /></a></div></div>");
